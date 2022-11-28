6fb124b299aba615c9394f80be7b87abb4957106
Nunthiphat <nunthiphat.b@kkumail.com>
2022-11-28 15:33:55 +0700

    add profile

---
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+// print string to commanline
+Console.WriteLine("Nunthiphat Butewang");
+// id
+Console.WriteLine("653450094-8");
+// email
+Console.WriteLine("Nunthiphat.b@kkumail.com");
