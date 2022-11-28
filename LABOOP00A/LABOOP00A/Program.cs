2fcad2ac4d21051e32f0e3a9208f226d5e4be212
Nunthiphat <nunthiphat.b@kkumail.com>
2022-11-28 16:50:41 +0700

    Most Final

--- LABOOP00A/LABOOP00A/Program.cs
+++ LABOOP00A/LABOOP00A/Program.cs
@@ -1,7 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-// print string to commanline
-Console.WriteLine("Nunthiphat Butewang");
-// id
-Console.WriteLine("653450094-8");
-// email
-Console.WriteLine("Nunthiphat.b@kkumail.com");
+using LABOOP00A;
+
+Students student1 = new Students();
+student1.name = "Nate";
+student1.weight = 60f;
+
+Students student2 = new Students();
+student2.name = "Pu";
+student2.weight = 69f;
+
+Students student3 = new Students();
+student3.name = "Toi";
+student3.weight = 63f;
+
+Students student4 = new Students();
+student4.name = "Not";
+student4.weight = 63f;
+
+Students student5 = new Students();
+student5.name = "Peaw";
+student5.weight = 39f;
+
+float avgWeight = (student1.weight + student2.weight + student3.weight + student4.weight + student5.weight) / 5;
+Console.WriteLine("Weight avg of Student : " + avgWeight);
